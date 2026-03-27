79f3ee642e4a698e5bc62c9d97249d160b8e0084
DimaCrafter <backup.dimacrafter@gmail.com>
2026-03-27 17:28:24 +0300

    feat: better GetTxByHash, GetCurrentBlock fix

--- blockchain/blockchain.go
+++ blockchain/blockchain.go
@@ -3,15 +3,17 @@ package blockchain
 import (
 	"context"
 	"fmt"
+	"net/url"
 
+	"gitlab.com/mayerdev/tatum-sdk-go/chain"
 	"gitlab.com/mayerdev/tatum-sdk-go/internal/httpclient"
 )
 
 type Service interface {
-	GetCurrentBlock(ctx context.Context, req CurrentBlockRequest) (*CurrentBlockResponse, error)
+	GetCurrentBlock(ctx context.Context, cn chain.ChainNetwork) (int64, error)
 	GetBlockByHash(ctx context.Context, req BlockByHashRequest) (*Block, error)
 	GetBlockByHeight(ctx context.Context, req BlockByHeightRequest) (*Block, error)
-	GetTxByHash(ctx context.Context, req TxByHashRequest) (*BlockchainTx, error)
+	GetTxByHash(ctx context.Context, req TxByHashRequest) (*TransactionInfo, error)
 	GetNativeBalance(ctx context.Context, req NativeBalanceRequest) (*NativeBalanceResponse, error)
 	GetBatchBalance(ctx context.Context, req BatchBalanceRequest) ([]NativeBalanceResponse, error)
 	GetMempool(ctx context.Context, req MempoolRequest) (*MempoolResponse, error)
@@ -25,9 +27,12 @@ func NewService(c *httpclient.Client) Service {
 	return &service{c: c}
 }
 
-func (s *service) GetCurrentBlock(ctx context.Context, req CurrentBlockRequest) (*CurrentBlockResponse, error) {
-	var out CurrentBlockResponse
-	return &out, s.c.Get(ctx, "/v4/blockchain/block/current", req.toQuery(), &out)
+func (s *service) GetCurrentBlock(ctx context.Context, cn chain.ChainNetwork) (int64, error) {
+	query := url.Values{}
+	query.Set("chain", string(cn))
+
+	var result int64
+	return result, s.c.Get(ctx, "/v4/data/blockchains/block/current", query, &result)
 }
 
 func (s *service) GetBlockByHash(ctx context.Context, req BlockByHashRequest) (*Block, error) {
@@ -40,9 +45,33 @@ func (s *service) GetBlockByHeight(ctx context.Context, req BlockByHeightRequest
 	return &out, s.c.Get(ctx, "/v4/blockchain/block/height", req.toQuery(), &out)
 }
 
-func (s *service) GetTxByHash(ctx context.Context, req TxByHashRequest) (*BlockchainTx, error) {
-	var out BlockchainTx
-	return &out, s.c.Get(ctx, "/v4/blockchain/transaction/hash", req.toQuery(), &out)
+func (s *service) GetTxByHash(ctx context.Context, req TxByHashRequest) (*TransactionInfo, error) {
+	var out TransactionInfo
+	var err error
+
+	ch, _ := req.Chain.Split()
+	if ch.IsUTXO() {
+		out.UTXO = &UTXOTransactionInfo{}
+		err = s.c.Get(ctx, "/v4/data/blockchains/transaction", req.toQuery(), out.UTXO)
+	} else if ch.IsEVM() {
+		out.EVM = &EVMTransactionInfo{}
+		err = s.c.Get(ctx, "/v4/data/blockchains/transaction", req.toQuery(), out.EVM)
+	} else if ch == chain.Solana {
+		out.Solana = &SolanaTransactionInfo{}
+		err = s.c.Get(ctx, "/v4/data/blockchains/transaction", req.toQuery(), out.Solana)
+	} else if ch == chain.Tron {
+		out.Tron = &TronTransactionInfo{}
+		err = s.c.Get(ctx, "/v4/data/blockchains/transaction", req.toQuery(), out.Tron)
+	} else {
+		return nil, fmt.Errorf("unsupported chain: %s", ch)
+	}
+
+	if err != nil {
+		return nil, err
+	}
+
+	out.Normalize()
+	return &out, err
 }
 
 func (s *service) GetNativeBalance(ctx context.Context, req NativeBalanceRequest) (*NativeBalanceResponse, error) {
--- blockchain/types.go
+++ blockchain/types.go
@@ -1,6 +1,10 @@
 package blockchain
 
-import "net/url"
+import (
+	"net/url"
+
+	"gitlab.com/mayerdev/tatum-sdk-go/chain"
+)
 
 type CurrentBlockRequest struct {
 	Chain string
@@ -51,22 +55,240 @@ type Block struct {
 
 type TxByHashRequest struct {
 	Hash  string
-	Chain string
+	Chain chain.ChainNetwork
 }
 
 func (r TxByHashRequest) toQuery() url.Values {
 	q := url.Values{}
 	q.Set("hash", r.Hash)
-	q.Set("chain", r.Chain)
+	q.Set("chain", string(r.Chain))
 	return q
 }
 
-type BlockchainTx struct {
+type EVMTransactionInfo struct {
+	Hash                 string `json:"hash"`
+	TransactionHash      string `json:"transactionHash"`
+	BlockHash            string `json:"blockHash"`
+	BlockNumber          int    `json:"blockNumber"`
+	From                 string `json:"from"`
+	To                   string `json:"to"`
+	Value                string `json:"value"`
+	Gas                  int    `json:"gas"`
+	GasPrice             string `json:"gasPrice"`
+	MaxFeePerGas         int    `json:"maxFeePerGas"`
+	MaxPriorityFeePerGas int    `json:"maxPriorityFeePerGas"`
+	Nonce                int    `json:"nonce"`
+	TransactionIndex     int    `json:"transactionIndex"`
+	Input                string `json:"input"`
+	Type                 int    `json:"type"`
+	AccessList           []any  `json:"accessList"`
+	ChainId              int    `json:"chainId"`
+	YParity              string `json:"yParity"`
+	GasUsed              int    `json:"gasUsed"`
+	EffectiveGasPrice    int    `json:"effectiveGasPrice"`
+	CumulativeGasUsed    int    `json:"cumulativeGasUsed"`
+	ContractAddress      string `json:"contractAddress"`
+	Status               bool   `json:"status"`
+	LogsBloom            string `json:"logsBloom"`
+	Logs                 []struct {
+		Address          string   `json:"address"`
+		Topics           []string `json:"topics"`
+		Data             string   `json:"data"`
+		BlockNumber      int      `json:"blockNumber"`
+		TransactionHash  string   `json:"transactionHash"`
+		TransactionIndex int      `json:"transactionIndex"`
+		BlockHash        string   `json:"blockHash"`
+		LogIndex         int      `json:"logIndex"`
+		Removed          bool     `json:"removed"`
+		BlockTimestamp   string   `json:"blockTimestamp"`
+	} `json:"logs"`
+	Fee            string `json:"fee"`
+	AdditionalProp any    `json:"additionalProp"`
+}
+
+type UTXOTransactionInfo struct {
+	BlockNumber int    `json:"blockNumber"`
+	Fee         int    `json:"fee"`
 	Hash        string `json:"hash"`
-	BlockNumber int64  `json:"blockNumber"`
-	From        string `json:"from"`
-	To          string `json:"to"`
-	Value       string `json:"value"`
+	Hex         string `json:"hex"`
+	Index       int    `json:"index"`
+	Inputs      []struct {
+		Prevout struct {
+			Hash  string `json:"hash"`
+			Index int    `json:"index"`
+		} `json:"prevout"`
+		Sequence int64  `json:"sequence"`
+		Script   string `json:"script"`
+		Coin     struct {
+			Version  int    `json:"version"`
+			Height   int    `json:"height"`
+			Value    int    `json:"value"`
+			Script   string `json:"script"`
+			Address  string `json:"address"`
+			Type     string `json:"type"`
+			ReqSigs  int    `json:"reqSigs"`
+			Coinbase bool   `json:"coinbase"`
+		} `json:"coin"`
+	} `json:"inputs"`
+	Locktime int `json:"locktime"`
+	Outputs  []struct {
+		Value        int    `json:"value"`
+		Script       string `json:"script"`
+		Address      string `json:"address"`
+		ScriptPubKey struct {
+			Type    string `json:"type"`
+			ReqSigs int    `json:"reqSigs"`
+		} `json:"scriptPubKey"`
+	} `json:"outputs"`
+	Size        int    `json:"size"`
+	Time        int    `json:"time"`
+	Version     int    `json:"version"`
+	Vsize       int    `json:"vsize"`
+	Weight      int    `json:"weight"`
+	WitnessHash string `json:"witnessHash"`
+	Block       string `json:"block"`
+}
+
+type SolanaTransactionInfo struct {
+	BlockTime int `json:"blockTime"`
+	Meta      struct {
+		ComputeUnitsConsumed int         `json:"computeUnitsConsumed"`
+		CostUnits            int         `json:"costUnits"`
+		Err                  interface{} `json:"err"`
+		Fee                  int         `json:"fee"`
+		InnerInstructions    []struct {
+			Index        int `json:"index"`
+			Instructions []struct {
+				Accounts       []int  `json:"accounts"`
+				Data           string `json:"data"`
+				ProgramIdIndex int    `json:"programIdIndex"`
+				StackHeight    int    `json:"stackHeight"`
+			} `json:"instructions"`
+		} `json:"innerInstructions"`
+		LoadedAddresses struct {
+			Readonly []interface{} `json:"readonly"`
+			Writable []interface{} `json:"writable"`
+		} `json:"loadedAddresses"`
+		LogMessages       []string `json:"logMessages"`
+		PostBalances      []int64  `json:"postBalances"`
+		PostTokenBalances []struct {
+			AccountIndex  int    `json:"accountIndex"`
+			Mint          string `json:"mint"`
+			Owner         string `json:"owner"`
+			ProgramId     string `json:"programId"`
+			UiTokenAmount struct {
+				Amount         string  `json:"amount"`
+				Decimals       int     `json:"decimals"`
+				UiAmount       float64 `json:"uiAmount"`
+				UiAmountString string  `json:"uiAmountString"`
+			} `json:"uiTokenAmount"`
+		} `json:"postTokenBalances"`
+		PreBalances      []int64 `json:"preBalances"`
+		PreTokenBalances []struct {
+			AccountIndex  int    `json:"accountIndex"`
+			Mint          string `json:"mint"`
+			Owner         string `json:"owner"`
+			ProgramId     string `json:"programId"`
+			UiTokenAmount struct {
+				Amount         string  `json:"amount"`
+				Decimals       int     `json:"decimals"`
+				UiAmount       float64 `json:"uiAmount"`
+				UiAmountString string  `json:"uiAmountString"`
+			} `json:"uiTokenAmount"`
+		} `json:"preTokenBalances"`
+		Rewards []interface{} `json:"rewards"`
+		Status  struct {
+			Ok interface{} `json:"Ok"`
+		} `json:"status"`
+	} `json:"meta"`
+	Slot        int `json:"slot"`
+	Transaction struct {
+		Message struct {
+			Header struct {
+				NumReadonlySignedAccounts   int `json:"numReadonlySignedAccounts"`
+				NumReadonlyUnsignedAccounts int `json:"numReadonlyUnsignedAccounts"`
+				NumRequiredSignatures       int `json:"numRequiredSignatures"`
+			} `json:"header"`
+			StaticAccountKeys    []string `json:"staticAccountKeys"`
+			RecentBlockhash      string   `json:"recentBlockhash"`
+			CompiledInstructions []struct {
+				ProgramIdIndex    int   `json:"programIdIndex"`
+				AccountKeyIndexes []int `json:"accountKeyIndexes"`
+				Data              struct {
+					Type string `json:"type"`
+					Data []int  `json:"data"`
+				} `json:"data"`
+			} `json:"compiledInstructions"`
+			AddressTableLookups []interface{} `json:"addressTableLookups"`
+		} `json:"message"`
+		Signatures []string `json:"signatures"`
+	} `json:"transaction"`
+	Version int `json:"version"`
+}
+
+type TronTransactionInfo struct {
+	Ret []struct {
+		ContractRet string `json:"contractRet"`
+	} `json:"ret"`
+	Signature        []string `json:"signature"`
+	BlockNumber      int      `json:"blockNumber"`
+	TxID             string   `json:"txID"`
+	NetFee           int      `json:"netFee"`
+	Fee              int      `json:"fee"`
+	EnergyFee        int      `json:"energyFee"`
+	EnergyUsage      int      `json:"energyUsage"`
+	EnergyUsageTotal int      `json:"energyUsageTotal"`
+	RawData          struct {
+		Contract []struct {
+			Parameter struct {
+				Value struct {
+					Data                  string `json:"data"`
+					OwnerAddress          string `json:"owner_address"`
+					ContractAddress       string `json:"contract_address"`
+					OwnerAddressBase58    string `json:"ownerAddressBase58"`
+					ContractAddressBase58 string `json:"contractAddressBase58"`
+				} `json:"value"`
+				TypeUrl string `json:"type_url"`
+			} `json:"parameter"`
+			Type string `json:"type"`
+		} `json:"contract"`
+		RefBlockBytes string `json:"ref_block_bytes"`
+		RefBlockHash  string `json:"ref_block_hash"`
+		Expiration    int64  `json:"expiration"`
+		FeeLimit      int    `json:"fee_limit"`
+		Timestamp     int64  `json:"timestamp"`
+	} `json:"rawData"`
+	Log []struct {
+		Address string   `json:"address"`
+		Topics  []string `json:"topics"`
+		Data    string   `json:"data"`
+	} `json:"log"`
+}
+
+type TransactionInfo struct {
+	Hash        string
+	BlockNumber int64
+
+	EVM    *EVMTransactionInfo
+	UTXO   *UTXOTransactionInfo
+	Solana *SolanaTransactionInfo
+	Tron   *TronTransactionInfo
+}
+
+func (tx *TransactionInfo) Normalize() {
+	if tx.UTXO != nil {
+		tx.Hash = tx.UTXO.Hash
+		tx.BlockNumber = int64(tx.UTXO.BlockNumber)
+	} else if tx.EVM != nil {
+		tx.Hash = tx.EVM.Hash
+		tx.BlockNumber = int64(tx.EVM.BlockNumber)
+	} else if tx.Solana != nil {
+		tx.Hash = tx.Solana.Transaction.Signatures[0]
+		tx.BlockNumber = int64(tx.Solana.Slot)
+	} else if tx.Tron != nil {
+		tx.Hash = tx.Tron.TxID
+		tx.BlockNumber = int64(tx.Tron.BlockNumber)
+	}
 }
 
 type NativeBalanceRequest struct {
